1a8f6b47d1cc0eba44bd9dd07d2c2a3e44d4eb5d
shehbazkp <shahbaz0313@gmail.com>
2025-02-19 15:54:43 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a704cc94d0d7649da8a5484c373c0106-283151249.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://a202a96cdca4145c48dd729598f27533-537984305.ap-south-1.elb.amazonaws.com',
 };
